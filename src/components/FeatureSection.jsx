085ef74cf36b70d2da24989d9f1ea7fccb16b5f7
smThilshan <smthilshan@gmail.com>
2025-06-30 13:57:12 +0400

    added feature section

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Title from './Title'
+import { dummyCarData } from '../assets/assets'
+import CarCard from './CarCard'
+
+const FeatureSection = () => {
+  return (
+    <div className='flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32'>
+        <div>
+            <Title  title='Featured Vehicles' subTitle="Explore our selection of premium vehicles available for your next adventure" />
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18'>
+            {dummyCarData.slice(0,6).map((car)=>(
+                <div key={car._id}>
+                    <CarCard car={car}/>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeatureSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeatureSection from '../components/FeatureSection'
 
 const Home = () => {
   return (
     <>
     <Hero/>
+    <FeatureSection/>
     </>
   )
 }
