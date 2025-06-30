8320fb8af30d7134b6b373d4bf97df1e58dca1c8
smThilshan <smthilshan@gmail.com>
2025-06-30 20:17:42 +0400

    added car search + screen

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -7,6 +7,11 @@ const CarDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [car, setCar] = useState(null);
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+  }
 
   useEffect(() => {
     setCar(dummyCarData.find((car) => car._id === id));
@@ -64,30 +69,68 @@ const CarDetails = () => {
               <p className="text-gray-500">{car.description}</p>
             </div>
 
-              <div>
-                <h1 className="text-xl font-medium mb-3">Features</h1>
-                <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-                  {
-                    [
-                      "360 Camera", "Blutooth", "GPS", "Heated Seats", "Rearview Mirror"
-                    ].map((item )=>(
-                      <li key={item} className="flex items-center text-gray-500">
-                        <img src={assets.check_icon} className="h-4 mrー2" alt="" />
-                        {item}
-                      </li>
-                    ))
-                  }
-                </ul>
-              </div>
-
+            <div>
+              <h1 className="text-xl font-medium mb-3">Features</h1>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                {[
+                  "360 Camera",
+                  "Blutooth",
+                  "GPS",
+                  "Heated Seats",
+                  "Rearview Mirror",
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img src={assets.check_icon} className="h-4 mrー2" alt="" />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
 
-        <form action=""></form>
+        <form
+         
+          onSubmit={handleSubmit}
+          className="shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500"
+        >
+          <p className="flex items-center justify-between text-2xl text-gray-800 font-semibold">
+            {currency}
+            {car.pricePerDay}
+            <span className="text-base text-gray-400 font-normal">
+              {" "}
+              per day
+            </span>
+          </p>
+          <hr className="border-borderColor my-6" />
+          <div className="flex flex-col gap-2">
+            <label htmlFor="pickup-date">Pickup Date</label>
+            <input
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg"
+              required
+              id="pickup-date"
+              min={new Date().toISOString().split("T")[0]}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="return-date">Return Date</label>
+            <input
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg"
+              required
+              id="return-date"
+             
+            />
+          </div>
+          <button className="w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer" >Book Now</button>
+          <p className="text-center text-sm">No credit card required to reserve </p>
+        </form>
       </div>
     </div>
   ) : (
-    <Loader/>
+    <Loader />
   );
 };
 
