db3746f8e3f331e074a2a53fd27f557f7d9d6e03
smThilshan <smthilshan@gmail.com>
2025-07-01 07:04:32 +0400

    added admin dashboard

--- src/components/owner/SidebarOwner.jsx
+++ src/components/owner/SidebarOwner.jsx
@@ -15,7 +15,7 @@ const SidebarOwner = () => {
     <div className='relative min-h-screen md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-borderColor text-sm'>
         <div className='group relative'>
             <label htmlFor="image">
-                <img src={image ? URL.createObjectURL(image) : user?.image || "https://www.shutterstock.com/image-vector/avatar-gender-neutral-silhouette-vector-600nw-2470054311.jpg"}  alt="" />
+                <img src={image ? URL.createObjectURL(image) : user?.image || "https://www.shutterstock.com/image-vector/avatar-gender-neutral-silhouette-vector-600nw-2470054311.jpg"}  className='h-12 md:h-14 w-12 md:w-14 rounded-full mx-auto' alt="" />
                 <input type="file" id='image' accept='image/*' hidden onChange={e=>setImage(e.target.files[0])} />
                 <div className='absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex items-center justify-center cursor-pointer'>
                     <img src={assets.edit_icon} alt="" />
