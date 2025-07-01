05c71351a25f205cb23250a0f8e329fcf6cd926a
smThilshan <smthilshan@gmail.com>
2025-07-01 22:50:16 +0400

    added manage cars

--- src/pages/owner/ManageBooking.jsx
+++ src/pages/owner/ManageBooking.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TitleOwner from '../../components/owner/TitleOwner'
 
 const ManageBooking = () => {
   return (
-    <div>ManageBooking</div>
+    <div className='px-4 pt-10 md:px-10 w-full'>
+      <TitleOwner title="Manage Bookings" subTitle="view all bookings and manage" />
+
+      <div className=''>
+
+      </div>
+
+    </div>
   )
 }
 
